5b0a14cfc78ed385408244fc61aed46b8abc3d09
Oshienga <malcolmoshienga@yahoo.com>
2023-09-07 05:48:00 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'This program prints a message to the standard output.'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	/** Print the message */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	/** Return 0 to indicate success */
+	return (0);
+}
