cbc158ab7b3e6a7603503537fc34fb0f81cc3d0c
Oshienga <malcolmoshienga@yahoo.com>
2023-09-11 11:22:21 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'This program prints a message to the standard output.'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 <= 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+		{
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
+
+			if (digit1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	/** Return 0 to indicate success */
+	return (0);
+}
