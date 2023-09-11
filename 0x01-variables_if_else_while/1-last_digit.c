835b65e1e4a3c82576425dc4a0b6f0a2ab420198
Oshienga <malcolmoshienga@yahoo.com>
2023-09-11 09:26:50 -0700

    Lastless

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,13 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n_last);
+		if (n_last == 0)
+		{
+			printf("Last digit of %d is %d and is 0\n", n, n_last);
+		} else
+		{
+			printf("Last digit of %d is %d and is greater than 5\n", n, n_last);
+		}
 	} else if (n < 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n_last);
