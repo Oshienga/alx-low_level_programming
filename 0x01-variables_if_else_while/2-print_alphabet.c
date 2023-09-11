3be1d5dcfea8109101b9672af34e2d5add7f8325
Oshienga <malcolmoshienga@yahoo.com>
2023-09-11 09:46:52 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'This program prints a message to the standard output.'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	for (char letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	/** Return 0 to indicate success */
+	return (0);
+}
