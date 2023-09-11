6143f5cfe0b8187cd1c634f3492ba533eac8a687
Oshienga <malcolmoshienga@yahoo.com>
2023-09-11 11:00:56 -0700

    TebAlpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'This program prints a message to the standard output.'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	/** Return 0 to indicate success */
+	return (0);
+}
