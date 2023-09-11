27d7a93b3928ef9987d61c2a990bf4b9769d250d
Oshienga <malcolmoshienga@yahoo.com>
2023-09-11 11:40:01 -0700

    Dabo dabo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'This program prints a message to the standard output.'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 <= 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+		{
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
+			putchar(' ');
+
+			for (digit3 = 0; digit2 <= 9; digit3++)
+			{
+				for (digit4 = digit3 + 1; digit4 <= 9; digit4++)
+				{
+					putchar(digit1 + '0');
+					putchar(digit4 + '0');
+				}
+			}
+
+			if (digit2 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	/** Return 0 to indicate success */
+	return (0);
+}
