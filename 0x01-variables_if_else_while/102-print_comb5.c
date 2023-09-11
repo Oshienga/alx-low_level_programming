90e4a4606198f882a727709bcceacbc7d4b55cbe
Oshienga <malcolmoshienga@yahoo.com>
2023-09-11 12:00:32 -0700

    three Threes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int num1, num2;
 
-	for (int num1 = 0; num1 <= 99; num1++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (int num2 = num1; num2 <= 99; num2++)
+		for (num2 = num1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
