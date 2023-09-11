983ef5c4a1156e82c2e4b4e294adef6ff55ab1bb
Oshienga <malcolmoshienga@yahoo.com>
2023-09-11 10:07:10 -0700

    No queues

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'This program prints a message to the standard output.'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+
+	putchar('\n');
+
+	/** Return 0 to indicate success */
+	return (0);
+}
