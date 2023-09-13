226cc6e841de790f0869a12ff5f2590e18946df6
Oshienga <malcolmoshienga@yahoo.com>
2023-09-12 17:46:30 -0700

    98 Problems

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98, separated by commas.
+ * Description: 'This program prints a message to the standard output.'
+ * @n: The starting number.
+ * Return: natural numbers
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
