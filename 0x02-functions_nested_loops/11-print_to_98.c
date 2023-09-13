e197453bb6153f82285afc24640af7bf5de8aa5c
Oshienga <malcolmoshienga@yahoo.com>
2023-09-12 18:10:48 -0700

    98 problems

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,36 +9,16 @@
 
 void print_to_98(int n)
 {
-	int i;
+	int i = n;
 
-	if (n <= 98)
+	while (i != 98)
 	{
-		for (i = n; i < 98; i++)
-		{
-			if (i < 10)
-			{
-				_putchar('0' + i);
-			} else
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
-		}
+		_putchar('0' + (i / 10) % 10);
+		_putchar('0' + i % 10);
+		_putchar(i == 98 ? '\n' : ',');
+		if (i < 98)
+			i++;
+		else
+			i--;
 	}
-	else
-	{
-		for (i = n; i > 98; i--)
-		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
 }
