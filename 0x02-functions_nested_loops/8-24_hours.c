bb7192569cb787d9ba913bc1fefbe507d2ae3a3e
Oshienga <malcolmoshienga@yahoo.com>
2023-09-12 17:10:05 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,6 +3,7 @@
 /**
  * jack_bauer - Prints every minute of the day of Jack Bauer.
  * Description: 'This program prints a message to the standard output.'
+ * Return: 24 hours.
  */
 
 void jack_bauer(void)
@@ -13,7 +14,12 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-			printf("%02d:%02d\n", hour, minute);
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
