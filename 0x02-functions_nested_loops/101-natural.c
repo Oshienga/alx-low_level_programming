408845b02471a9b325aec85e7d0fba632faf39d4
Oshienga <malcolmoshienga@yahoo.com>
2023-09-14 02:52:32 -0700

    Natural 3,5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'This program prints a message to the standard output.'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int result = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			result += i;
+		}
+	}
+
+	printf("%d", result)
+	printf("\n");
+
+	/** Return 0 to indicate success */
+	return (0);
+}
