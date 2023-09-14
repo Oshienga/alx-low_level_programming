429c22862c5bfe2bb83913687b57e17b241c8795
Oshienga <malcolmoshienga@yahoo.com>
2023-09-14 03:48:09 -0700

    Fibo what?

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,18 +9,19 @@
 int main(void)
 {
 	int fib[50];
+	int i;
 
 	fib[0] = 1;
 	fib[1] = 2;
 
 	/** Calculate and store the first 50 Fibonacci numbers */
-	for (int i = 2; i < 50; i++)
+	for (i = 2; i < 50; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 	}
 
 	/** Print the first 50 Fibonacci numbers separated by comma and space */
-	for (int i = 0; i < 49; i++)
+	for (i = 0; i < 49; i++)
 	{
 		printf("%d, ", fib[i]);
 	}
