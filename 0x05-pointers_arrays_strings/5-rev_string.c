17f19825879731af89d62e103f50e1ceef6d615a
Oshienga <malcolmoshienga@yahoo.com>
2023-09-18 07:55:43 -0700

    Reversed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,24 @@ void rev_string(char *s)
 {
 	int length = 0;
 	int i;
+	int j;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	for (i = length - 1; i >= 0; i--)
+	for (i = 0; i < length; i++)
 	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
+
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
 }
