f336204c0a22bc9136617a6a238552805214ff5f
Oshienga <malcolmoshienga@yahoo.com>
2023-09-18 07:47:17 -0700

    Rever

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,23 +9,15 @@ void rev_string(char *s)
 {
 	int length = 0;
 	int i;
-	int j;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	for (i = 0; i < length; i++)
+	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
-
-	for (j = length - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
-	_putchar('\n');
-	_putchar('\0');
 }
