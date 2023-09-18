9266f0c646a8f6addfffbf9a85cb67c5ee1a7d3b
Oshienga <malcolmoshienga@yahoo.com>
2023-09-18 08:37:05 -0700

    Arrayser

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,15 @@
 
 void print_array(int *a, int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		printf("\n");
 		return;
 	}
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 		if (i < n - 1)
