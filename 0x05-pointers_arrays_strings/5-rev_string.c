97d69516094b2acc89b587140b33c2516cfa4cae
Oshienga <malcolmoshienga@yahoo.com>
2023-09-18 07:12:08 -0700

    Reverer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: A pointer to the string to be printed in reverse.
+ */
+
+void rev_string(char *s);
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
