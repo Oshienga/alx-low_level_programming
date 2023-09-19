5453522298fc31c0c811c490963c1a590e6626ff
Oshienga <malcolmoshienga@yahoo.com>
2023-09-19 06:01:47 -0700

    StrToInt

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -12,23 +13,24 @@ int _atoi(char *s)
 	int sign = 1;
 	int i = 0;
 
-	while (s[i] == ' ')
+	while (s[i])
 	{
-		i++;
-	}
-
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
+		if (s[i] == '-')
+			sign = -sign;
+		if (s[i] == '-' || s[i] == '+')
+			i++;
+		if (s[i] >= '0' && s[i] <= '9')
+			break;
 		i++;
 	}
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
+		if (result == INT_MAX / 10 && (s[i] - '0') > (INT_MAX % 10))
+		{
+			return ((sign == 1) ? INT_MAX : INT_MIN);
+		}
+
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
