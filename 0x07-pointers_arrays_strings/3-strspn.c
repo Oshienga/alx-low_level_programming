61a8d4435dc1ed3c673ba1eca4413b024e490771
Oshienga <malcolmoshienga@yahoo.com>
2023-09-25 08:13:01 -0700

    Janta ni janta

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * Description: 'This program prints a message to the standard output.'
+ * @s: first argument
+ * @accept: second argument
+ * Return: result
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int found;
+
+	while (*s != '\0')
+	{
+		found = 0;
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				found = 1;
+				break;
+			}
+			a++;
+		}
+
+		if (!found)
+		{
+			break;
+		}
+
+		count++;
+		s++;
+	}
+
+	return (count);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * Description: 'This program prints a message to the standard output.'
+ * @s: first argument
+ * @accept: second argument
+ * Return: result
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
