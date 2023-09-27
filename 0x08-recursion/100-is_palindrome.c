e25dd02c05609b024cec9156b4cd6242ab4f3d7a
Oshienga <malcolmoshienga@yahoo.com>
2023-09-27 11:26:01 -0700

    Palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,52 +1,50 @@
 #include "main.h"
 
 /**
- * is_palindrome - Entry point
+ * str_length - subfunction point
  * Description: 'This program prints a message to the standard output.'
- * @s: first argument
+ * @s: subfunction argument
+ * Return: result
+*/
+
+int str_length(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * is_palindrome - main point
+ * Description: 'This program prints a message to the standard output.'
+ * @s: subfunction argument
  * Return: result
 */
 
 int is_palindrome(char *s)
 {
 	int length;
+	int i;
 
-/**
- * str_length - subfunction point
- * Description: 'This program prints a message to the standard output.'
- * @str: subfunction argument
- * Return: result
-*/
-
-	int str_length(char *str)
-	{
-		if (*str == '\0')
-		{
-			return (0);
-		}
-		return (1 + str_length(str + 1));
-	}
-
-/**
- * is_palindrome_helper - subfunction point
- * Description: 'This program prints a message to the standard output.'
- * @start: subfunction argument
- * @end: subfunction argument
- * Return: result
-*/
-
-	int is_palindrome_helper(char *start, char *end)
-	{
-		if (start >= end)
-		{
-			return (1);
-		}
-		if (*start != *end)
-		{
-			return (0);
-		}
-		return (is_palindrome_helper(start + 1, end - 1));
-	}
 	length = str_length(s);
-	return (is_palindrome_helper(s, s + length - 1));
+
+	if (length == 0)
+	{
+		return (1);
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		if (s[i] != s[length - 1 - i])
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
