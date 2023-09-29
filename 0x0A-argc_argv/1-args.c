75f8b671bcd87521b8c7e8f2ee811d82c74e9b7d
Oshienga <malcolmoshienga@yahoo.com>
2023-09-29 09:15:36 -0700

    arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,13 +10,15 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
+	int num = argc;
+
+	if (num == 1)
 	{
 		printf("0\n");
 	}
 	else
 	{
-		printf("%d\n", argc - 1);
+		printf("%d\n", num - 1);
 	}
 	return (0);
 }
