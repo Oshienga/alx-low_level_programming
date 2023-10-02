b549dde6110ef3b3bd6aafa26616988b6aeb292d
Oshienga <malcolmoshienga@yahoo.com>
2023-10-02 05:58:52 -0700

    Changed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,13 +11,14 @@
 
 int main(int argc, char *argv[])
 {
+	int cents, coins25, coins10, coins5, coins2, coins1, totalCoins;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
@@ -25,27 +26,17 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	int coins25 = cents / 25;
-
+	coins25 = cents / 25;
 	cents %= 25;
-
-	int coins10 = cents / 10;
-
+	coins10 = cents / 10;
 	cents %= 10;
-
-	int coins5 = cents / 5;
-
+	coins5 = cents / 5;
 	cents %= 5;
-
-	int coins2 = cents / 2;
-
+	coins2 = cents / 2;
 	cents %= 2;
-
-	int coins1 = cents;
-
-	int totalCoins = coins25 + coins10 + coins5 + coins2 + coins1;
+	coins1 = cents;
+	totalCoins = coins25 + coins10 + coins5 + coins2 + coins1;
 
 	printf("%d\n", totalCoins);
-
 	return (0);
 }
