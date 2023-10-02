60df54951e8d15232c08597a7341b6c4211a6aff
Oshienga <malcolmoshienga@yahoo.com>
2023-10-02 07:26:27 -0700

    Walk

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,13 +13,14 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
+	int **grid;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	int **grid = (int **)malloc(height * sizeof(int *));
+	grid = (int **)malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 	{
