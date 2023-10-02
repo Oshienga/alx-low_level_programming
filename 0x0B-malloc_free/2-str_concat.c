33624e8ed1988757cacf55b7d9f61914356e6aef
Oshienga <malcolmoshienga@yahoo.com>
2023-10-02 07:22:52 -0700

    Conc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,8 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	char *result;
+
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -24,7 +26,7 @@ char *str_concat(char *s1, char *s2)
 	size_t len1 = strlen(s1);
 	size_t len2 = strlen(s2);
 
-	char *result = (char *)malloc(len1 + len2 + 1);
+	result = (char *)malloc(len1 + len2 + 1);
 
 	if (result == NULL)
 	{
