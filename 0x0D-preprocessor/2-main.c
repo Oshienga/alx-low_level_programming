b7d7e92ed73a084829b84b2c68bbabb441760b34
Oshienga <malcolmoshienga@yahoo.com>
2023-10-09 04:08:22 -0700

    Header

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: 'This program prints something'
+ * Return: return
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
