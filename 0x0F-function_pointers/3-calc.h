ad52b734f6d37495e30399424be43ef134bccec9
Oshienga <malcolmoshienga@yahoo.com>
2023-10-11 12:03:57 -0700

    Calculator

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -7,6 +7,10 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+typedef struct {
+	char *op;
+	int (*f)(int, int);
+} op_t;
 int (*get_op_func(char *s))(int, int);
 
 #endif /* MAIN_H */
