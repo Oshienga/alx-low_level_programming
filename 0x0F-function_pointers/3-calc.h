9d77ca88c77a78850bf8fdd7005d777f7e8af42e
Oshienga <malcolmoshienga@yahoo.com>
2023-10-12 06:18:13 -0700

    Typedef

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -7,10 +7,19 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-typedef struct {
+
+/**
+ * struct op - yep
+ * Description: 'it is a typedef'
+ * @op: operator
+ * @f: function
+ */
+typedef struct op
+{
 	char *op;
 	int (*f)(int, int);
 } op_t;
+
 int (*get_op_func(char *s))(int, int);
 
 #endif /* MAIN_H */
