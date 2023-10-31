7616b87ee199e000b548cfdf19cc3eaa99904106
Oshienga <malcolmoshienga@yahoo.com>
2023-10-31 05:31:05 -0700

    Files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,29 +16,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	FILE *file = fopen(filename, "r");
+	FILE *file;
+	char *buffer;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
 
+	file = fopen(filename, "r");
 	if (file == NULL)
 	{
 		return (0);
 	}
-	char *buffer = (char *)malloc(letters);
-
+	buffer = (char *)malloc(letters);
 	if (buffer == NULL)
 	{
 		fclose(file);
 		return (0);
 	}
-	ssize_t bytes_read = fread(buffer, 1, letters, file);
-
+	bytes_read = fread(buffer, 1, letters, file);
 	if (bytes_read == -1)
 	{
 		free(buffer);
 		fclose(file);
 		return (0);
 	}
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written != bytes_read)
 	{
 		free(buffer);
