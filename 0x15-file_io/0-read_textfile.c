2cd24bebb2118065bcb386f2a3c80edf8a7c86b5
Oshienga <malcolmoshienga@yahoo.com>
2023-11-03 13:32:20 -0700

    Life

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,25 +17,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		return (0);
 	}
-
 	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (o == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-
+	r = read(o, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(o);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || w != r)
+	{
+		free(buffer);
+		close(o);
+		return (0);
+	}
 	free(buffer);
 	close(o);
-
 	return (w);
 }
